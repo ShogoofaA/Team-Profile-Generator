2400df6b0c0061e0bb2d5219b69fc61bb2ccbfc9
Shogoofa Akbari <shogoofa.musket@gmail.com>
2022-06-01 17:02:31 -0700

    created prompt questions

--- index.js
+++ index.js
@@ -1,2 +1,134 @@
 const fs = require('fs');
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
+class Prompt {
+    constructor(teamData) {
+        this.employees = [];
+    }
+    promptUser() {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What is the Team Managers name?',
+                    name: 'name',
+            
+                },
+                {
+                    type: 'input',
+                    message: 'What is the Team Managers ID?',
+                    name: 'id',
+            
+                },
+                {
+                    type: 'input',
+                    message: 'What is the Team Managers email?',
+                    name: 'email',
+            
+                },
+                {
+                    type: 'input',
+                    message: 'What is the Team Managers office number?',
+                    name: 'officeNumber',
+            
+                },
+            ])
+            .then(({ name, id, email, officeNumber}) => {
+                this.employees.push(new Manager(name, id, email, officeNumber));
+                this.getEngineer()
+            })
+    }
+        getEngineer() {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'What is the name of your Engineer?',
+                    name: 'name',
+            
+                },
+                {
+                    type: 'input',
+                    message: 'What is your Engineers ID number?',
+                    name: 'id',
+            
+                },
+                {
+                    type: 'input',
+                    message: 'What is the Engineers email?',
+                    name: 'email',
+            
+                },
+                {
+                    type: 'input',
+                    message: 'What is the Engineers GitHub Username?',
+                    name: 'gitHub',
+                },
+        ])
+        .then(({ name, id, email, gitHub}) => {
+            this.employees.push(new Engineer(name, id, email, gitHub));
+            this.getEmployee()
+        })
+    }
+    getEmployee() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the name of your Employee?',
+                name: 'name',
+        
+            },
+            {
+                type: 'input',
+                message: 'What is your Employees ID number?',
+                name: 'id',
+        
+            },
+            {
+                type: 'input',
+                message: 'What is the Employees email?',
+                name: 'email',
+        
+            },
+        ])
+        .then(({ name, id, email}) => {
+            this.employees.push(new Employee(name, id, email));
+            this.getIntern()
+        })
+    }
+    getIntern() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the name of your Intern?',
+                name: 'name',
+        
+            },
+            {
+                type: 'input',
+                message: 'What is your Interns ID number?',
+                name: 'id',
+        
+            },
+            {
+                type: 'input',
+                message: 'What is your Interns email?',
+                name: 'email',
+        
+            },
+            {
+                type: 'input',
+                message: 'What school did your Intern attend?',
+                name: 'school',
+            },
+        ])
+        .then(({ name, id, email, school}) => {
+            this.employees.push(new Intern(name, id, email, school));
+        })
+    }
+}
+
+module.exports = new Prompt().promptUser();
